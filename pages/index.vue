0482b51e0e2066868337ced61419b6fb20b85196
affan <affan.tdev@gmail.com>
2021-07-22 00:07:37 +0700

    Connect API

--- pages/index.vue
+++ pages/index.vue
@@ -1,123 +1,176 @@
 <template>
-<!-- <Tutorial /> -->
-<div class="container p-2">
+  <!-- <Tutorial /> -->
+  <div class="container">
     <div class="d-flex flex-row justify-content mx-auto">
-        <div class="col-md-8">
-            <form @submit.prevent="sub">
-                <div class="text-center">
-                    <h1 class="text-3xl">TO DO LIST</h1>
-                    <div class="mb-3 mt-3">
-                        <input v-model="todo" type="text" placeholder="Add a Todo" class="rounded form-control bg-gray-200 mt-2 p-1" />
-                        <button type="submit" class="rounded bg-blue-500 text-white text-xl p-1">Add tesk</button>
-                    </div>
-                </div>
-            </form>
-            <div class="flex justify-center">
-                <div class="flex flex-col w-1/2 justify-center  shadow-md bg-blue-50">
-                    <div v-for="(todos, index) in todolist" :key="index">
-                        <!-- {{ todos.isEdit }} -->
-                        <input v-if="todos.isEdit" v-model="update" type="text" class="bg-blue-50 w-3/4"/>
-                        <label v-else class="w-3/4">{{ todos.tesk }}</label>
-                        <button class="ml-5" @click="editTodo(index, update, todos.isEdit)">Edit</button>
-                        <button class="ml-2" @click="removeTodo(index)">Delete</button>
-                        
-                        
-                        
-                        
-                        <!-- Check Function -->
-                        <!-- <div v-if="!todos.isChack">
-                        <div @click="chackTodo(taskindex)">
-                            <i class="fas fa-check">
-
-                            </i>
-                        </div>
-                    </div> -->
-                        <!-- <button class="ml-5" @click="chackTodo(taskindex, chack)">Check</button> -->
-                    </div>
-                </div>
+      <div class="col-md-8">
+        <form @submit.prevent="addTodo">
+          <div class="text-center">
+            <h1 class="text-3xl">TO DO LIST</h1>
+            <div class="mb-3 mt-3">
+              <input
+                v-model="todo"
+                type="text"
+                placeholder="Add a Todo"
+                class="rounded form-control bg-gray-200 mt-2 p-1"
+              />
+              <button
+                type="submit"
+                class="rounded bg-blue-500 text-white text-xl p-1"
+              >
+                Add tesk
+              </button>
             </div>
+          </div>
+        </form>
+        <div class="flex justify-center">
+          <div
+            class="
+              flex flex-col
+              w-1/2
+              justify-center
+              text-center
+              shadow-md
+              bg-blue-50
+            "
+          >
+            <div v-for="(todos, index) in todolist" :key="index">
+              <input
+                v-if="todos.is_edited"
+                v-model="update"
+                type="text"
+                class="bg-blue-50 w-3/4"
+                @keypress.enter="editTodo(index)"
+              />
+              <label v-else class="w-3/4">{{ todos.content }}</label>
+              <button
+                class="ml-3 bg-green-200 rounded"
+                @click="editTodo(index)"
+              >
+                Edit
+              </button>
 
+              <button
+                class="ml-3 bg-green-200 rounded"
+                @click="toggleEdit(index)"
+              >
+                OpenEdit
+              </button>
+
+              <button
+                class="ml-2 bg-red-500 rounded"
+                @click="removeTodo(todos.task_id)"
+              >
+                Delete
+              </button>
+
+              <!-- Check Function -->
+              <!-- <div v-if="!todos.isCheck">
+                            <div @click="checkTodo(taskindex)">
+                                <i class="fas fa-check"></i>
+                            </div>
+                        </div>
+
+                        <button class="" @click="checkTodo(taskindex, check)">Check</button>
+                    </div> -->
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    // layout: 'default',
-    data() {
-        return {
-            todo: '',
-            isEdit: false,
-            isChack: false,
-            update: null,
+  // layout: 'default',
+  data() {
+    return {
+      todo: '',
+      isEdit: false,
+      isCheck: false,
+      update: null,
+    }
+  },
+  computed: {
+    todolist() {
+      return this.$store.getters['todolist/gettersTodo']
+    },
+  },
+
+  async created() {
+    await this.getTask()
+  },
+
+  methods: {
+    async getTask() {
+      await this.$store.dispatch('todolist/getToDoListFromAPI')
+    },
+
+    async addTodo() {
+      const newtodo = {
+        content: this.todo,
+        is_checked: false,
+        is_edited: false,
+      }
+      const newtodos = [...this.todolist, newtodo]
+      if (this.todo) {
+        await this.$store.dispatch('todolist/setToDoListFromAPI', {
+          newTodos: newtodos,
+        })
+        await this.getTask()
+      }
+    },
+
+  async removeTodo(taskindex) {
+      await this.$store.dispatch('todolist/deleteToDoListFromAPI', taskindex)
+      await this.getTask()
+      alert('Delete Successfully')
+    },
+
+    // checkTodo(taskindex, todo, check) {
+    //     const checkToDoList = this.todolist.map((thing, index) => {
+    //         if (index === taskindex) {
+    //             return (thing = {
+    //                 tesk: todo,
+    //                 isCheck: !thing.check,
+    //             });
+    //         } else {
+    //             return thing;
+    //         }
+    //     });
+    //     this.$store.commit('todolist/chackTodo', checkToDoList)
+
+    // },
+
+    async editTodo(taskindex) {
+      const editToDoList = this.todolist.map((thing, index) => {
+        if (index === taskindex) {
+          return (thing = {
+            content: this.update,
+            is_checked: thing.is_checked,
+          })
+        } else {
+          return thing
         }
+      })
+      if (this.update) {
+        await this.$store.dispatch('todolist/setToDoListFromAPI', {
+          newTodos: editToDoList,
+        })
+      }
     },
-    computed: {
-        todolist() {
-            return this.$store.getters['todolist/gettersTodo']
-        },
+    toggleEdit(index) {
+      this.$store.commit('todolist/editTodo', index)
     },
-
-    created() {
-        // this.todolist = this.$store.getters['todolist/gettersTodo']
-    },
-
-    mounted() {},
-
-    methods: {
-        sub() {
-            if (this.todo) {
-                this.$store.commit('todolist/addTodo', this.todo)
-                this.todo = ''
-            }
-        },
-        removeTodo(index) {
-            this.$store.commit('todolist/removeTodo', index)
-            alert("Delete Successfully")
-        },
-
-        chackTodo(taskindex, todo, chack) {
-            const chackToDoList = this.todolist.map((thing, index) => {
-                if (index === taskindex) {
-                    return (thing = {
-                        tesk: todo,
-                        isChack: !chack,
-                    });
-                } else {
-                    return thing;
-                }
-            });
-            this.$store.commit('todolist/chackTodo', chackToDoList)
-
-        },
-
-        editTodo(taskindex, todo, edit) {
-            this.isEdit = !this.isEdit
-            // console.log(edit)
-            const editToDoList = this.todolist.map((thing, index) => {
-                if (index === taskindex) {
-                    return (thing = {
-                        tesk: todo,
-                        isEdit: !edit,
-                    })
-                } else {
-                    return thing
-                }
-            })
-            // console.log(editToDoList)
-            this.$store.commit('todolist/editTodo', editToDoList)
-            this.update = null;
-        },
-    },
+  },
 }
 </script>
 
 <style scoped>
 .add {
-    margin-top: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- store/todolist.js
+++ store/todolist.js
@@ -1,18 +1,7 @@
-export const state = () => ({
+// const access_Token = localStorage.getItem("access_Token");
 
-    todos:
-        [
-            {
-                isEdit: false,
-                isChack: false,
-                tesk: "Work",
-            },
-            {
-                isEdit: false,
-                isChack: false,
-                tesk: "Computers",
-            },
-        ]
+export const state = () => ({
+    todos: []
 })
 
 export const mutations = {
@@ -20,7 +9,6 @@ export const mutations = {
     addTodo(state, payload) {
         const result = { isEdit: false, tesk: payload }
         state.todos.push(result)
-        alert("Add Task complete")
     },
 
     removeTodo(state, payload) {
@@ -29,15 +17,80 @@ export const mutations = {
     },
 
     editTodo(state, payload) {
-        state.todos = payload
-
+        const editToDoList = state.todos.map((thing, index) => {
+            if (index === payload) {
+                return (thing = {
+                    is_edited: !thing.is_edited
+                })
+            } else {
+                return thing
+            }
+        });
+        state.todos = editToDoList;
     },
-    chackTodo(state, payload){
+
+    chackTodo(state, payload) {
         state.todos = payload
-    }
+    },
 
+    setTaskFromAPI(state, data) {
+        if (data !== null) {
+            const mutantData = data.map(thing => {
+                return (thing = {
+                    content: thing.content,
+                    is_checked: thing.is_checked,
+                    task_id: thing.task_id,
+                    is_edited: false
+                });
+            });
+            state.todos = mutantData;
+        }
+    },
+};
 
-}
+export const actions = {
+    getToDoListFromAPI(state) {
+        this.$axios
+            .$get(`/api/dashboard/14`, {
+                headers: {
+                    Authorization: `Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+                }
+            })
+            .then(res => {
+                state.commit("setTaskFromAPI", res.data);
+            })
+    },
+
+    setToDoListFromAPI(state, data) {
+        const newtodo = data.newTodos;
+        const newTodo = newtodo.map(thing => {
+            return (thing = {
+                content: thing.content,
+                is_checked: thing.is_checked,
+            });
+        });
+
+        this.$axios
+            .$post(`/api/dashboard/14`, {
+                data: newTodo
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+                }
+            }).then(res => {
+                // alert("Add Task Complete")
+            })
+    },
+    async deleteToDoListFromAPI(state, payload) {
+        await this.$axios.$delete("api/dashboard/" + payload, {
+            headers: {
+                Authorization: `Bearer aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+            }
+        });
+    },
+};
+
 export const getters = {
     gettersTodo: (state) => state.todos
 }
